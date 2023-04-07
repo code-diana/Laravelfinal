43166974d403ea31ba82ff05af13e7360194ad06
diana120500 <diana.martos@inslapineda.cat>
2023-04-07 12:14:50 +0200

    Diana07

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInitf0b8c59c0c417d404cf7a825a110aca1
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInitf0b8c59c0c417d404cf7a825a110aca1::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
