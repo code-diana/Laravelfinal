27fe1e1cef67503352fb93747bcf53282c232871
diana120500 <diana.martos@inslapineda.cat>
2023-04-10 19:55:20 +0200

    Di

--- resources/views/clasificacionesvista.blade.php
+++ resources/views/clasificacionesvista.blade.php
@@ -14,18 +14,29 @@
                     <table class="table">
                     <thead>
                         <tr>
+                        <th scope="col"></th>
                         <th scope="col">Posicion</th>
                         <th scope="col">Puntos</th>
                         <th scope="col">Nombre</th>
                         <th scope="col">Apellido</th>
                         </tr>
                     </thead>
-                    <tbody>
+                    <tbody class="clasifications">
                         <?php $cont=0; ?>
                         @foreach($runners as $f)
                         <?php
                         if($cont<10){ ?>
                             <tr>
+                                @if ($cont==0)
+                                <td><img src="../resources/img/copa-de-oro.png" class="copas"></td>
+                                @elseif ($cont==1)
+                                <td><img src="../resources/img/copa-de-plata.png" class="copas"></td>
+                                @elseif($cont==2)
+                                <td><img src="../resources/img/copa-de-bronce.png" class="copas"></td>
+                                @else
+                                <td><img src="../resources/img/medalla.png" class="medalla"></td>
+                                @endif
+
                             <th scope="row"><?php echo $cont+1; ?></th>
                             <td>{{$f['points']}}</td>
                             <td>{{$f['name']}}</td>
