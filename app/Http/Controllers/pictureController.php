a8c64340b42f166a0c73e566080bc401ffc8fb3f
diana120500 <diana.martos@inslapineda.cat>
2023-04-12 07:10:58 +0200

    ultimodetodos

--- app/Http/Controllers/pictureController.php
+++ app/Http/Controllers/pictureController.php
@@ -11,39 +11,39 @@ use Illuminate\Support\Str;
 class pictureController extends Controller
 {
     
-    // public function uploadF(Request $request){
-    //     $carrera = Race::find($request->id);
-    //     if ($request->isMethod('post')){
+    public function uploadF(Request $request){
+        $carrera = Race::find($request->id);
+        if ($request->isMethod('post')){
 
-    //         Picture::create([
-    //         'race_id' => $request->id,
-    //         'image' => request('image'),
-    //         ]);
+            Picture::create([
+            'race_id' => $request->id,
+            'image' => request('image'),
+            ]);
 
-    //         //lo de subir la imagen... importante multipart/form-data
-    //         if($request->hasFile('image')){
-    //             $imagen = $request->file('image');
+            //lo de subir la imagen... importante multipart/form-data
+            if($request->hasFile('image')){
+                $imagen = $request->file('image');
 
-    //             //aquí le asignamos el nombre
-    //             $nombreimagen = Str::slug($request->file('image')).".".$imagen->guessExtension();
-    //             //y la ruta
-    //             $ruta = public_path("../resources/img/");
+                //aquí le asignamos el nombre
+                $nombreimagen = Str::slug($request->file('image')).".".$imagen->guessExtension();
+                //y la ruta
+                $ruta = public_path("../resources/img/");
     
-    //             //$imagen->move($ruta,$nombreimagen);
-    //             copy($imagen->getRealPath(),$ruta.$nombreimagen);     
-    //         }
+                //$imagen->move($ruta,$nombreimagen);
+                copy($imagen->getRealPath(),$ruta.$nombreimagen);     
+            }
 
-    //         $carrera = Race::all();
-    //         return redirect()->route('editarCarrera' , [
-    //             'carreras'=>$carrera
-    //         ]); 
-    //     }      
-    //     else{
-    //         return view('admin.carreras.subirFotos' ,[
-    //             'carreras' => $carrera
-    //         ]);
-    //     }
-    // }
+            $carrera = Race::all();
+            return redirect()->route('editarCarrera' , [
+                'carreras'=>$carrera
+            ]); 
+        }      
+        else{
+            return view('admin.carreras.subirFotos' ,[
+                'carreras' => $carrera
+            ]);
+        }
+    }
 
     // public function uploadF(Request $request){
     //     $carrera = Race::find($request->id);
@@ -67,29 +67,6 @@ class pictureController extends Controller
     //         ]);
     //     }
     // }
-
-    public function uploadF(Request $request){
-        $carrera = Race::find($request->id);
-        if ($request->isMethod('post')){
-            if ($request->hasFile('file')) {
-                $imagenes = $request->file('file')->store('public/imagenes');//hay que crear la carpeta imagenes en storage/app/public
-                //storage/app/public/imagenes/WNw6F1QxYxihB5jK02pyn6KtQwLOnCaeFqisT0yR.jpg
-                $url = Storage::url($imagenes);
-                $newString = str_replace("/storage/", "/storage/app/public/", $url);
-                echo $newString;
-
-                Picture::create([
-                    'race_id' => $request->id,
-                    'image' => $newString,
-                ]);
-            }
-        }      
-        else{
-            return view('admin.carreras.subirFotos' ,[
-                'carreras' => $carrera
-            ]);
-        }
-    }
 
     public function viewF(Request $request){
         $carrera = Race::find($request->id);
